f9925648e7dbe900a3cd4a5aa5c2b2e46573b3fa
나연 <kny5579@naver.com>
2025-05-01 22:15:31 +0900

    feat(#17): test빌드 오류 해결

--- src/test/java/com/couponmoa/backend/couponmoacoupon/CouponmoaCouponApplicationTests.java
+++ src/test/java/com/couponmoa/backend/couponmoacoupon/CouponmoaCouponApplicationTests.java
@@ -1,10 +1,28 @@
 package com.couponmoa.backend.couponmoacoupon;
 
+import com.couponmoa.backend.couponmoacoupon.domain.coupon.repository.SearchHistoryRepository;
+import com.couponmoa.backend.couponmoacoupon.domain.coupon.repository.SearchRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchRepositoriesAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@EnableAutoConfiguration(exclude = {
+        ElasticsearchDataAutoConfiguration.class,
+        ElasticsearchRepositoriesAutoConfiguration.class
+})
 class CouponmoaCouponApplicationTests {
+
+    @MockitoBean
+    private SearchRepository searchRepository;
+
+    @MockitoBean
+    private SearchHistoryRepository searchHistoryRepository;
 
     @Test
     void contextLoads() {
