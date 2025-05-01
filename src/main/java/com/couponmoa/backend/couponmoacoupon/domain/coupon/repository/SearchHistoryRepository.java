3719f741e2a0510e2f5cbc52d33a6dd9ad910202
myeongje <tiqmtiqm6541@gmail.com>
2025-05-01 16:22:04 +0900

    feat:#8 findByUserId 메서드 추가

--- src/main/java/com/couponmoa/backend/couponmoacoupon/domain/coupon/repository/SearchHistoryRepository.java
+++ src/main/java/com/couponmoa/backend/couponmoacoupon/domain/coupon/repository/SearchHistoryRepository.java
@@ -3,6 +3,9 @@ package com.couponmoa.backend.couponmoacoupon.domain.coupon.repository;
 import com.couponmoa.backend.couponmoacoupon.domain.coupon.entity.SearchHistory;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.util.List;
+
 public interface SearchHistoryRepository extends ElasticsearchRepository<SearchHistory, String> {
+    List<SearchHistory> findByUserId(String userId);
 }
 
